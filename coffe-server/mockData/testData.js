72e429c3cee35a55c3675e6aab2d110f6dd4e701
halid <halit1as@gmail.com>
2021-09-13 01:37:59 +0300

    test for getById implemented

---
+++ coffe-server/mockData/testData.js
@@ -0,0 +1,4 @@
+module.exports.singleCoffeById = {
+    id:'613545eb22d6d3280c78ae16',
+    fakeId:'fakeONE'
+}
--- coffe-server/test/coffeeTest.js
+++ coffe-server/test/coffeeTest.js
@@ -6,9 +6,15 @@ const should = require('chai').should();
 const expect = require('chai').expect;
 const server = require('../app')
 
+const {singleCoffeById } = require('../mockData/testData')
+
+
 //Assertion
 chai.should()
 chai.use(chaiHttp);
+
+
+
 
 describe('Coffee API', () => {
 
@@ -34,8 +40,49 @@ describe('Coffee API', () => {
                     done();
                 });
         });
+    });
+
+    /**
+     * Test the GET coffee (by id)
+     */
+    describe("GET /coffee/:id", () => {
+        it("It should GET a single coffee by ID", (done) => {
+            const coffeeId  = singleCoffeById.id
+            chai.request(server)
+                .get("/coffee/" + coffeeId)
+                .end((err, response) => {
+                    response.should.have.status(200);
+                    response.body.should.be.a('object');
+                    response.body.should.have.property('code');
+                    response.body.should.have.property('success');
+                    response.body.should.have.property('message');
+                    response.body.should.have.property('coffee');
+                    response.body.coffee.should.be.a('object');
+                    expect(response.body.success).to.equal(true);
+                    expect(response.body.code).to.equal(200);
+                    done();
+                });
+        });
+
+        it("It should NOT GET a single coffee by fakeID", (done) => {
+            const coffeeId = singleCoffeById.fakeId
+            chai.request(server)
+                .get("/coffee/" + coffeeId)
+                .end((err, response) => {
+
+                    response.should.have.status(404);
+                    response.body.should.be.a('object');
+                    response.body.should.have.property('code');
+                    response.body.should.have.property('message');
+                    response.body.should.have.property('error');
+                    expect(response.body.code).to.equal(404);
+                    expect(response.body.message).to.equal("Not Found");
+                    done();
+                });
+        });
 
     });
+
 
 
 
